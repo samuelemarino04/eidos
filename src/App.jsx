48112de2755446bb34cea2ee76bfb98f99f6bd32
Samuele <samuele12601@gmail.com>
2025-06-28 22:38:54 +0200

    secondo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return <Navigation />;
+}
+
+export default App;
